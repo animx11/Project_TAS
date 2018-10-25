6f433980571619be95ddebdf8daf68fa65642214
animx11 <animx11@gmail.com>
2018-10-25 22:14:56 +0200

    Dodanie interfejsu UserService

---
+++ src/main/java/Spring/Services/UsersService.java
@@ -0,0 +1,19 @@
+package Spring.Services;
+
+import Spring.Entities.Users;
+
+public interface UsersService {
+
+    Users getById(Integer id);
+    Users save(Users user);
+    void delete(Integer id);
+
+    Iterable<Users> getByUserName(String userName);
+    Iterable<Users> getByPermissionGroup(String userPermissionGroup);
+    Boolean checkIfExist(Integer id);
+    Boolean checkIfExistUserNameWithPassword(String userName, String password);
+
+    Iterable<Users> listUsers();
+    Iterable<Users> listAllPages(Integer pages, Integer howManyOn);
+    long howManyRows();
+}
