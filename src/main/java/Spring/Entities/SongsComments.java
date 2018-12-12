d3a70a65c83704fa65c47385331542252a515c84
bargal1@st.amu.edu.pl <Szkieleton9879>
2018-12-12 18:43:10 +0100

    Klasy komentarzy do utworów, zespołów i albumów

---
+++ src/main/java/Spring/Entities/SongsComments.java
@@ -0,0 +1,41 @@
+package Spring.Entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "SongsComments", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"id"})
+})
+public class SongsComments{
+    @GeneratedValue
+    @Id
+    @Column(name = "id")
+    private int id;
+
+    @OneToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "User", referencedColumnName = "id")
+    private Users userName;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "rating")
+    private int rating;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "Songs", referencedColumnName = "id")
+    private Songs songs;
+
+    public SongsComments() {
+    }
+
+    public SongsComments(String content, int rating) {
+        this.content = content;
+        this.rating = rating;
+    }
+
+    public int getId() {return id;}
+    public String getContent() {return content;}
+    public int getRating() {return rating;}
+}
